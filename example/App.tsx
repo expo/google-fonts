04ce41888206527faa06c0b5a322bf4db9045dd1
Charlie Cheever <ccheever@gmail.com>
2021-06-30 10:59:49 -0400

    Upgrading example to SDK 41

--- example/App.tsx
+++ example/App.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-import { AppLoading } from 'expo';
+import AppLoading from 'expo-app-loading';
 import { useFonts, BalsamiqSans_400Regular } from '@expo-google-fonts/balsamiq-sans';
 import {
   SourceSansPro_400Regular_Italic,
