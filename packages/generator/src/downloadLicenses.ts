e1eb67419f996b82a71d532043c2e9300ee72ae2
Kadi Kraman <kadi@expo.io>
2025-06-05 08:30:43 +0100

    Update packages/generator/src/downloadLicenses.ts
    Co-authored-by: Jakub Grzywacz <kontakt@jakubgrzywacz.pl>

--- packages/generator/src/downloadLicenses.ts
+++ packages/generator/src/downloadLicenses.ts
@@ -93,7 +93,7 @@ async function checkLicense(font: FontItem) {
       'Edu NSW ACT Hand',
       'Edu VIC WA NT Hand',
       'Edu NSW ACT Cursive',
-      'Edu NSW ACT Hand Pre'
+      'Edu NSW ACT Hand Pre',
     ].includes(font.family)
   ) {
     return {
