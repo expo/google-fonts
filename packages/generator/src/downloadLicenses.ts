a189b973cf664d928cabb4c5fae434b562d086c0
Jakub Grzywacz <kontakt@jakubgrzywacz.pl>
2025-12-02 13:08:56 +0100

    fix: extract license overrides to separate files and fix missing licenses (#206)

--- packages/generator/src/downloadLicenses.ts
+++ packages/generator/src/downloadLicenses.ts
@@ -4,6 +4,7 @@ import PQueue from 'p-queue';
 import path from 'path';
 
 import { FontLicensesDir, FontLicenseTypes } from './constants';
+import { licenseOverrides } from './licenseOverrides';
 import { FontItem } from './types';
 import { download } from './utils/download';
 import { varNameForWebfont } from './utils/name';
@@ -44,63 +45,10 @@ export async function downloadLicenses(fonts: FontItem[]) {
   }
 }
 
-const materialLicense =
-  'https://raw.githubusercontent.com/google/material-design-icons/refs/heads/master/LICENSE';
-
-const materialIcons = [
-  'Material Symbols',
-  'Material Icons Two Tone',
-  'Material Icons Sharp',
-  'Material Icons Round',
-  'Material Icons Outlined',
-  'Material Icons',
-  'Material Symbols Outlined',
-  'Material Symbols Rounded',
-  'Material Symbols Sharp',
-];
-
 async function checkLicense(font: FontItem) {
-  if (materialIcons.includes(font.family)) {
-    return { type: FontLicenseTypes.Apache, url: materialLicense, family: font.family };
-  }
-
-  // No license file
-  // It's marked as OFL and the other font by the same author has the license file so we'll reuse it
-  if (font.family === 'Kumar One Outline') {
-    return {
-      type: FontLicenseTypes.OFL,
-      url: 'https://raw.githubusercontent.com/google/fonts/refs/heads/main/ofl/kumarone/OFL.txt',
-      family: font.family,
-    };
-  }
-
-  // No license file
-  // It's marked as OFL and the other font by the same author has the license file so we'll reuse it
-  if (font.family === 'M PLUS Rounded 1c') {
-    return {
-      type: FontLicenseTypes.OFL,
-      url: 'https://raw.githubusercontent.com/google/fonts/refs/heads/main/ofl/mpluscodelatin/OFL.txt',
-      family: font.family,
-    };
-  }
-
-  // missing license file for the new fonts form the same author
-  if (
-    [
-      'Edu VIC WA NT Hand Pre',
-      'Edu QLD Hand',
-      'Edu SA Hand',
-      'Edu NSW ACT Hand',
-      'Edu VIC WA NT Hand',
-      'Edu NSW ACT Cursive',
-      'Edu NSW ACT Hand Pre',
-    ].includes(font.family)
-  ) {
-    return {
-      type: FontLicenseTypes.OFL,
-      url: 'https://raw.githubusercontent.com/google/fonts/refs/heads/main/ofl/edunswacthand/OFL.txt',
-      family: font.family,
-    };
+  const override = licenseOverrides.find(([names]) => names.includes(font.family))?.[1];
+  if (override) {
+    return { ...override, family: font.family };
   }
 
   const name = font.family.replace(/ /g, '').toLowerCase();
@@ -139,6 +87,10 @@ async function checkAndDownloadLicense(font: FontItem) {
     throw new Error(`License not found for ${font.family}`);
   } else {
     const filepath = path.join(FontLicensesDir, varNameForWebfont(font) + '_' + license.type);
-    await download(filepath, license.url);
+    if (license.url) {
+      await download(filepath, license.url);
+    } else if (license.content) {
+      await fsExtra.writeFile(filepath, license.content);
+    }
   }
 }
