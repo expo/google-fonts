f9d9b1996c5ac1a2eb55defc65dcd5e53b68f5bb
Jakub Grzywacz <jakub.grzywacz@swmansion.com>
2025-12-02 11:16:38 +0100

    fix: extract license overrides to separate files and fix missing licenses

---
+++ packages/generator/src/licenseOverrides.ts
@@ -0,0 +1,160 @@
+import { FontLicenseTypes } from './constants';
+
+export const licenseOverrides: [string[], { type: string; url?: string; content?: string }][] = [
+  [
+    [
+      'Material Symbols',
+      'Material Icons Two Tone',
+      'Material Icons Sharp',
+      'Material Icons Round',
+      'Material Icons Outlined',
+      'Material Icons',
+      'Material Symbols Outlined',
+      'Material Symbols Rounded',
+      'Material Symbols Sharp',
+    ],
+    {
+      type: FontLicenseTypes.Apache,
+      url: 'https://raw.githubusercontent.com/google/material-design-icons/refs/heads/master/LICENSE',
+    },
+  ],
+  [
+    ['Kumar One Outline'],
+    {
+      type: FontLicenseTypes.OFL,
+      url: 'https://raw.githubusercontent.com/google/fonts/refs/heads/main/ofl/kumarone/OFL.txt',
+    },
+  ],
+  [
+    ['M PLUS Rounded 1c'],
+    {
+      type: FontLicenseTypes.OFL,
+      url: 'https://raw.githubusercontent.com/google/fonts/refs/heads/main/ofl/mpluscodelatin/OFL.txt',
+    },
+  ],
+  [
+    ['Edu QLD Hand'],
+    {
+      type: FontLicenseTypes.OFL,
+      url: 'https://raw.githubusercontent.com/SorkinType/QLDSchoolHandAustralia/refs/heads/main/OFL.txt',
+    },
+  ],
+  [
+    ['Edu SA Hand'],
+    {
+      type: FontLicenseTypes.OFL,
+      url: 'https://raw.githubusercontent.com/SorkinType/SASchoolHandAustralia/refs/heads/main/OFL.txt',
+    },
+  ],
+  [
+    ['Edu VIC WA NT Hand', 'Edu VIC WA NT Hand Pre'],
+    {
+      type: FontLicenseTypes.OFL,
+      url: 'https://raw.githubusercontent.com/SorkinType/VICWANTSchoolhandAustralia/refs/heads/main/OFL.txt',
+    },
+  ],
+  [
+    ['Edu NSW ACT Cursive', 'Edu NSW ACT Hand Pre'],
+    {
+      type: FontLicenseTypes.OFL,
+      url: 'https://raw.githubusercontent.com/SorkinType/NSWACTSchoolHandAustralia/refs/heads/main/OFL.txt',
+    },
+  ],
+  [
+    ['Google Sans Flex'],
+    {
+      type: FontLicenseTypes.OFL,
+      content: `This Font Software is licensed under the SIL Open Font License, Version 1.1.
+This license is copied below, and is also available with a FAQ at:
+https://openfontlicense.org
+
+
+-----------------------------------------------------------
+SIL OPEN FONT LICENSE Version 1.1 - 26 February 2007
+-----------------------------------------------------------
+
+PREAMBLE
+The goals of the Open Font License (OFL) are to stimulate worldwide
+development of collaborative font projects, to support the font creation
+efforts of academic and linguistic communities, and to provide a free and
+open framework in which fonts may be shared and improved in partnership
+with others.
+
+The OFL allows the licensed fonts to be used, studied, modified and
+redistributed freely as long as they are not sold by themselves. The
+fonts, including any derivative works, can be bundled, embedded, 
+redistributed and/or sold with any software provided that any reserved
+names are not used by derivative works. The fonts and derivatives,
+however, cannot be released under any other type of license. The
+requirement for fonts to remain under this license does not apply
+to any document created using the fonts or their derivatives.
+
+DEFINITIONS
+"Font Software" refers to the set of files released by the Copyright
+Holder(s) under this license and clearly marked as such. This may
+include source files, build scripts and documentation.
+
+"Reserved Font Name" refers to any names specified as such after the
+copyright statement(s).
+
+"Original Version" refers to the collection of Font Software components as
+distributed by the Copyright Holder(s).
+
+"Modified Version" refers to any derivative made by adding to, deleting,
+or substituting -- in part or in whole -- any of the components of the
+Original Version, by changing formats or by porting the Font Software to a
+new environment.
+
+"Author" refers to any designer, engineer, programmer, technical
+writer or other person who contributed to the Font Software.
+
+PERMISSION & CONDITIONS
+Permission is hereby granted, free of charge, to any person obtaining
+a copy of the Font Software, to use, study, copy, merge, embed, modify,
+redistribute, and sell modified and unmodified copies of the Font
+Software, subject to the following conditions:
+
+1) Neither the Font Software nor any of its individual components,
+in Original or Modified Versions, may be sold by itself.
+
+2) Original or Modified Versions of the Font Software may be bundled,
+redistributed and/or sold with any software, provided that each copy
+contains the above copyright notice and this license. These can be
+included either as stand-alone text files, human-readable headers or
+in the appropriate machine-readable metadata fields within text or
+binary files as long as those fields can be easily viewed by the user.
+
+3) No Modified Version of the Font Software may use the Reserved Font
+Name(s) unless explicit written permission is granted by the corresponding
+Copyright Holder. This restriction only applies to the primary font name as
+presented to the users.
+
+4) The name(s) of the Copyright Holder(s) or the Author(s) of the Font
+Software shall not be used to promote, endorse or advertise any
+Modified Version, except to acknowledge the contribution(s) of the
+Copyright Holder(s) and the Author(s) or with their explicit written
+permission.
+
+5) The Font Software, modified or unmodified, in part or in whole,
+must be distributed entirely under this license, and must not be
+distributed under any other license. The requirement for fonts to
+remain under this license does not apply to any document created
+using the Font Software.
+
+TERMINATION
+This license becomes null and void if any of the above conditions are
+not met.
+
+DISCLAIMER
+THE FONT SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO ANY WARRANTIES OF
+MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT
+OF COPYRIGHT, PATENT, TRADEMARK, OR OTHER RIGHT. IN NO EVENT SHALL THE
+COPYRIGHT HOLDER BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+INCLUDING ANY GENERAL, SPECIAL, INDIRECT, INCIDENTAL, OR CONSEQUENTIAL
+DAMAGES, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+FROM, OUT OF THE USE OR INABILITY TO USE THE FONT SOFTWARE OR FROM
+OTHER DEALINGS IN THE FONT SOFTWARE.`,
+    },
+  ],
+];
